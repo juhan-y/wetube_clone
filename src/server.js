5209cdaf148aa948209d5b5061384eb799cb4bf5
juhan-y <sju03460@kookmin.ac.kr>
2022-02-11 13:03:22 +0900

    middleware concept upload
    applying middleware concept

--- src/server.js
+++ src/server.js
@@ -16,9 +16,23 @@ const app = express(); // express application 생성.
 // 홈페이지가 계속 로딩중인 것이다.
 // 만약 브라우저에서 "localhost:4000/lalala"에 접속한다면 Cannot GET /lalala라고 뜰 것이다.
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
 const handleHome = (req, res) => {
   //   return res.send("I still love you"); // request를 종료.
-  return res.end();
+  return res.send("I love middlewares");
   // res.end()는 request를 종료하는 방법중 하나이다.
 };
 // req, res는 express에 의해서 받은 것이다. express가 없었다면
@@ -28,7 +42,19 @@ const handleLogin = (req, res) => {
   return res.send("Login here.");
 };
 
-app.get("/", handleHome);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger); // app.use는 global middleware!
+// 대신 순서가 중요하다. app.get을 먼저 사용하고 app.use를 실행하면
+// app.get의 인자인 route에 접속할 때는 middleware가 작동하지않는다.
+// (그 route 이외의 route는 똑같이 적용됨)
+app.use(privateMiddleware);
+app.get("/", handleHome); // handleHome은 finalware
+// 이 때 gossipMiddleware은 middleWare이다.
+// next()를 호출하는 함수는 middleware!!
+app.get("/protected", handleProtected);
 app.get("/login", handleLogin);
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 //외부접속 listen -> 외부세계에 open
