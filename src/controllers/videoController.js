af5268139fb59176b04b6de254c180b920eeb6f0
juhan-y <sju03460@kookmin.ac.kr>
2022-02-16 17:49:03 +0900

    220216 ~#6.1 upload
    ~ #6.1  nomadcoder
    ternary operator,
    videos/id, videos/id/edit web page design.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,38 @@
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", {
-    pageTitle: "Home",
-    potato: "tomato",
-    fakeUser,
-  });
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+  const id = req.params.id;
+  // ES6 방식 : const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  upload,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 // parameters (순서 중요!)
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 // :id -> variable
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
