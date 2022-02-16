475dff38b8e780e9f3077d29cc36eff89f005f9e
juhan-y <sju03460@kookmin.ac.kr>
2022-02-16 11:53:17 +0900

    template conditionals upload
    nomad coder ~# 5.7
    conditionals concept in templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,16 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: false,
 };
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit");
-};
+
+export const trending = (req, res) =>
+  res.render("home", {
+    pageTitle: "Home",
+    potato: "tomato",
+    fakeUser,
+  });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ const PORT = 4000;
 
 const app = express();
 const loggerMiddleware = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(loggerMiddleware);
 
 app.use("/", globalRouter);
