92f5cfa6fbb222e9813e49da22f3904be665f2d6
juhan-y <sju03460@kookmin.ac.kr>
2022-02-19 23:46:01 +0900

    Creating account upload little modified

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,6 +15,6 @@ export const postJoin = async (req, res) => {
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
+export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See");
