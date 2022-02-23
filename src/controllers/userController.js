19723380df97f4ecdf415a8973184d071c2419f4
juhan-y <sju03460@kookmin.ac.kr>
2022-02-23 17:24:15 +0900

    220223 Login finished Logout upload
    github login finished
    logout finished

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,7 +39,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
@@ -110,18 +110,35 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    const email = emailData.find(
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!email) {
+    if (!emailObj) {
       return res.redirect("/login");
     }
-    console.log(emailData);
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      const user = await User.create({
+        avatarUrl: userData.avatar_url,
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      console.log("Created!");
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
 };
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   edit,
-  remove,
   see,
   logout,
   startGithubLogin,
@@ -10,7 +9,6 @@ import {
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get("/logout", logout);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
