8b8e7530fb37509ad4772791dd5d1a43a3d4d476
juhan-y <sju03460@kookmin.ac.kr>
2022-02-24 00:33:04 +0900

    220223 edit profile, protect middleware
    ~ #8.1

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,27 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
+  // 로그인을 안한 상태라면 req.session.user는 undefined인데
+  // "|| {}" 없이 그냥 넘겨주면 pug에서 undefined로 인한 오류가 발생함!
+  // 이 때문에 아래의 protectMiddleware가 발동되지 않으므로
+  // "|| {}"를 작성한 것!
   console.log(res.locals);
   next(); // next를 해야 다음 app.use나 app.get이 실행될 수 있음!
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,17 +1,20 @@
 import express from "express";
 import {
-  edit,
+  getEdit,
+  postEdit,
   see,
   logout,
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 const userRouter = express.Router();
 
-userRouter.get("/edit", edit);
-userRouter.get("/logout", logout);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+// all -> 어떤 http method에도 적용하겠다는 말
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 
 userRouter.get("/:id", see);
 
