77b549d46973a8da39a0a2a64a3ad7263c5a68df
juhan-y <sju03460@kookmin.ac.kr>
2022-03-07 14:10:28 +0900

    script upload 3

--- src/init.js
+++ src/init.js
@@ -7,9 +7,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
-
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(PORT, handleListening);
+app.listen(process.env.PORT || 4000, handleListening);
